09d2c71b4785d83349e5ad4058f3ce977981c4c1
DongwookYoon <dy252@cornell.edu>
2014-12-07 11:04:38 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var SERVER_URL = 'https://richreview.net/';
+SERVER_URL = 'https://localhost:8000/';
+
+function HandleError(error){
+    console.log('fail', error);
+    $('#div_error').load('error.html');
+}
+
+function GetRawData(){
+    try{
+        var url = SERVER_URL+'dataviewer?op=getRawData';
+        var posting = $.post(url, '');
+        posting.success(function(result){
+            console.log('success: ', result);
+        });
+        posting.fail(function(error){
+            HandleError(error);
+        });
+    }
+    catch(error){
+        HandleError(error);
+    }
+}
+
+function Run(){
+    GetRawData()
+}
+
+Run();
